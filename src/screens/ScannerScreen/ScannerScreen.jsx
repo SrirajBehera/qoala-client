1d7b5a74a142ed77f7b7b7f64ea5dca28538bdbc
Sriraj Behera <behera.sriraj2911@gmail.com>
2023-12-25 16:23:08 +0530

    added check if file already exists on storage

--- src/screens/ScannerScreen/ScannerScreen.jsx
+++ src/screens/ScannerScreen/ScannerScreen.jsx
@@ -40,75 +40,93 @@ const ScannerScreen = () => {
     e.preventDefault();
     const file = e.target[0]?.files[0];
     if (!file) return;
-    const maxSizeInBytes = 2 * 1024 * 1024; // 2MB in bytes
-    const fileSize = file.size;
-    if (fileSize > maxSizeInBytes) {
-      // fileSize > 2MB then show popup message
-      alert(
-        `File size is too large, please upload an image with a size less than 2MB.\nSelected File Size: ${
-          fileSize / 1024 / 1024
-        }MB`
-      );
-      return;
-    }
 
-    // Upload file and metadata to the object 'images/mountains.jpg'
-    // const storageRef = ref(storage, "images/" + file.name);
-    // const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+    // Check if the file with the same name already exists
     const storageRef = ref(storage, `${userDBId}/${file.name}`);
-    const uploadTask = uploadBytesResumable(storageRef, file);
+    getDownloadURL(storageRef)
+      .then((existingDownloadURL) => {
+        // File with the same name already exists
+        alert(
+          "File with the same name already exists. Either rename the file and try again or upload a new file."
+        );
+      })
+      .catch((error) => {
+        // File not found, proceed with the upload
+        if (error.code === "storage/object-not-found") {
+          const maxSizeInBytes = 2 * 1024 * 1024; // 2MB in bytes
+          const fileSize = file.size;
+          if (fileSize > maxSizeInBytes) {
+            // fileSize > 2MB then show popup message
+            alert(
+              `File size is too large, please upload an image with a size less than 2MB.\nSelected File Size: ${
+                fileSize / 1024 / 1024
+              }MB`
+            );
+            return;
+          }
 
-    // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-        setProgresspercent(progress);
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
+          const storageRef = ref(storage, `${userDBId}/${file.name}`);
+          const uploadTask = uploadBytesResumable(storageRef, file);
+
+          // Listen for state changes, errors, and completion of the upload.
+          uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+              const progress =
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log("Upload is " + progress + "% done");
+              setProgresspercent(progress);
+              switch (snapshot.state) {
+                case "paused":
+                  console.log("Upload is paused");
+                  break;
+                case "running":
+                  console.log("Upload is running");
+                  break;
+              }
+            },
+            (error) => {
+              // A full list of error codes is available at
+              // https://firebase.google.com/docs/storage/web/handle-errors
+              switch (error.code) {
+                case "storage/unauthorized":
+                  // User doesn't have permission to access the object
+                  console.log(
+                    "User doesn't have permission to access the object"
+                  );
+                  alert("User doesn't have permission to access the object");
+                  break;
+                case "storage/canceled":
+                  // User canceled the upload
+                  console.log("User canceled the upload");
+                  alert("User canceled the upload");
+                  break;
+
+                // ...
+
+                case "storage/unknown":
+                  // Unknown error occurred, inspect error.serverResponse
+                  console.log("Unknown error occurred: ", error.serverResponse);
+                  alert("Unknown error occurred");
+                  break;
+              }
+            },
+            () => {
+              // Upload completed successfully, now we can get the download URL
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                console.log("File available at", downloadURL);
+                setImgUrl(downloadURL);
+                runOCR(downloadURL);
+              });
+            }
+          );
+        } else {
+          // Handle other errors
+          console.error("Error checking if file exists:", error);
+          alert("Error checking if file exists");
         }
-      },
-      (error) => {
-        // A full list of error codes is available at
-        // https://firebase.google.com/docs/storage/web/handle-errors
-        switch (error.code) {
-          case "storage/unauthorized":
-            // User doesn't have permission to access the object
-            console.log("User doesn't have permission to access the object");
-            alert("User doesn't have permission to access the object");
-            break;
-          case "storage/canceled":
-            // User canceled the upload
-            console.log("User canceled the upload");
-            alert("User canceled the upload");
-            break;
-
-          // ...
-
-          case "storage/unknown":
-            // Unknown error occurred, inspect error.serverResponse
-            console.log("Unknown error occurred: ", error.serverResponse);
-            alert("Unknown error occurred");
-            break;
-        }
-      },
-      () => {
-        // Upload completed successfully, now we can get the download URL
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at", downloadURL);
-          setImgUrl(downloadURL);
-          runOCR(downloadURL);
-        });
-      }
-    );
+      });
   };
 
   const token = localStorage.getItem("@jwt-token");
@@ -177,7 +195,9 @@ const ScannerScreen = () => {
         <span>a</span>
         <span>i</span>
       </span>
-      <h3 className={styles.underline_effect}>Scan your Thailand National ID Card through OCR</h3>
+      <h3 className={styles.underline_effect}>
+        Scan your Thailand National ID Card through OCR
+      </h3>
       <div className={styles.subContainer}>
         <div className={styles.left}>
           <span>Allowed image (PNG, JPEG, JPG) with a 2MB size limit</span>
